cfa0538a2b1c59e9f2585760c0c5f99486a8db12
Yure Couto <49762865+yurecouto@users.noreply.github.com>
2023-03-13 14:19:36 -0300

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	//"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -30,11 +29,4 @@ func main() {
 
 	utils.Log("Golang API Listening and Serving!")
 	http.ListenAndServe(config.Port, r)
-}
-
-func MeuMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), "user", "123")
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
 }
